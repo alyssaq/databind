1666c97fefbf56a22fcfb37e99332342a0c1e8ee
alyssaq <alyssaquek@gmail.com>
2013-12-07 22:40:49 +0800

    Message bus / history stack for late subscribers

--- scripts/pubsub.js
+++ scripts/pubsub.js
@@ -5,17 +5,30 @@ var pubsub = (function() {
     pub: function(id) {
       var args = [].slice.call(arguments, 1);
 
-      cache[id] = cache[id] || [];
+      if (!cache[id]) {
+        cache[id] = {
+          callbacks: [],
+          args: [args]
+        }
+      }
 
-      for (var i = 0, il = cache[id].length; i < il, i++) {
-        cache[id][i].apply(null, args);
+      for (var i = 0, il = cache[id].callbacks.length; i < il; i++) {
+        cache[id].callbacks[i].apply(null, args);
       }
     },
     sub: function(id, fn) {
       if (!cache[id]) {
-        cache[id] = [fn]
+        cache[id] = {
+          callbacks: [fn],
+          args: []
+        }
       } else {
-        cache[id].push(fn);
+        cache[id].callbacks.push(fn);
+      }
+
+      //Execute past published messages to late subs
+      for (var i = 0, il = cache[id].args.length; i < il; i++) {
+        fn.apply(null, cache[id].args[i]);
       }
     },
     unsub: function(id, fn) {
