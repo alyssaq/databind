495816e1ab4e77dde3ddc826f014d9da9621886e
alyssaq <alyssaquek@gmail.com>
2013-12-07 22:24:23 +0800

    Added unsub to unsubscribe published callbacks

--- scripts/pubsub.js
+++ scripts/pubsub.js
@@ -5,7 +5,7 @@ var pubsub = (function() {
     pub: function(id) {
       var args = [].slice.call(arguments, 1);
 
-      if (!cache[id]) cache[id] = [];
+      cache[id] = cache[id] || [];
 
       for (var i = 0, il = cache[id].length; i < il, i++) {
         cache[id][i].apply(null, args);
@@ -16,6 +16,18 @@ var pubsub = (function() {
         cache[id] = [fn]
       } else {
         cache[id].push(fn);
+      }
+    },
+    unsub: function(id, fn) {
+      if (!id) return;
+
+      if (!fn) {
+        cache[id] = [];
+      } else {
+        index = cache[id].indexOf(fn);
+        if (index > -1) {
+          cache[id] = cache[id].slice(0, index).concat(cache[id].slice(index + 1));
+        }
       }
     }
   }
